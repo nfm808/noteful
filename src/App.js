c2d968f66f94b408948403db9e1fa695dbf60ee6
Nathan Fowler <luxurysandbox@gmail.com>
2019-04-29 22:05:23 -1000

    created routes for main sidebar view

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
 import STORE from './dummy-store'
 import Header from './components/header/header'
+import Sidebar from './components/sidebar/sidebar';
+import MainSidebar from './components/sidebar/main_sidebar/mainSidebar';
+import NoteViewSidebar from './components/sidebar/note_sidebar/noteViewSidebar';
 import './App.css'
 
 class App extends Component {
@@ -11,10 +14,32 @@ class App extends Component {
 
   
   render() {
+    const { folders, notes } = this.state.store;
+    console.log(folders)
     return (
-      <section className="app">
+      <div className="app">
         <Header title="Noteful" />
-      </section>
+        <Sidebar store={this.state.store}>
+          <Route 
+            exact 
+            path='/' 
+            render={() =>
+              <MainSidebar
+                list={folders}
+              />
+            } 
+          />
+          <Route 
+            path='/folder/:id'
+            render={(props) => 
+              <MainSidebar 
+                active={props.match.params.id.slice(1)}
+                list={folders}
+              />} 
+          />
+        </Sidebar>
+
+      </div>
     )
   }
 }
